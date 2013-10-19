c293159adc98e1ae49f3bc4e1ae004fd7307a6b6
rissole <bonzaibrother@gmail.com>
2013-10-19 14:21:28 +1100

    added dependency input, and wrote a nice generalised output commend

--- js/eventcreator.js
+++ js/eventcreator.js
@@ -1,12 +1,52 @@
+// $(function) is shorthand for $.ready(function)
+// i.e. run 'function' once the page has loaded all elements
 $(function() {
 
+// On Submit
 $('#eventForm').submit(function() {
     var output = {};
-    var ename = $('input[name=ename]').val();
-    output['ename'] = ename;
+    
+    // EVENT_FIELDS defined in eventdata.js contains all the fields of an event.
+    $.each(EVENT_FIELDS, function(i, field) {
+        // search for all visible controls with that field name.
+        var fieldControls = $('[name='+field+']:visible');
+        
+        // if there is only one, its field value is merely the control's value.
+        if (fieldControls.length == 1) {
+            output[field] = fieldControls.val();
+        }
+        // if there are many, its field value is an array of the controls' values.
+        else if (fieldControls.length != 0) {
+            
+            output[field] = [];
+            fieldControls.each(function(i, fieldControl) {
+                output[field].push(fieldControl.val());
+            });
+        }
+        // if there are 0 controls... bummer.
+        else {
+            console.error("ERROR - could not find field control for '"+field+"'");
+        }
+    });
+    
     $('#output textarea').val(JSON.stringify(output));
     $('#output').show();
     return false;
 });
 
+// Populate Places dropdown
+$.each(EVENT_PLACE, function(i, place) {
+    $('#placeDropDown').append('<option>' + place + '</option>');
 });
+
+// 'Add Dependency' on click
+$('#addDependencyButton').click(function() {
+    $('#dependencyControls').append($('#dependencyControls > div:first').clone().show());
+});
+
+});
+
+// Dependency delete button on click
+function remove_dependency(elem) {
+    $(elem).parent('.control-group').remove();
+}
